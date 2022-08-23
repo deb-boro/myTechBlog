1008b8574e54841cde249a64183d8425efa75082
deb boro <debajit.boro666@gmail.com>
2022-08-22 23:27:40 -0400

    initial files

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS my_tech_blog_db;
+
+CREATE DATABASE my_tech_blog_db;
+
+use my_tech_blog_db
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const routes = require('./controllers/')
+const sequelize = require('./config/connection')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers })
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+}
+
+const app = express()
+const PORT = process.env.PORT || 3001
+app.use(session(sess))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+//The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets. This is useful for front-end specific files like images, style sheets, and JavaScript files.
+app.use(express.static(path.join(__dirname, 'public')))
+
+//turn on routes
+app.use(routes)
+
+//turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now Listening'))
+})
